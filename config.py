e7d2ee86ed9a1e8c6afa4c20d2b4e71b609397e2
brettvanderwerff <34728357+brettvanderwerff@users.noreply.github.com>
2018-01-23 16:51:24 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+'''This config module contains all the sensitive configuration values needed to run the send_email module
+and the ebay_buy_it_now_search_tool.
+'''
+
+api_key = 'Your ebay API key goes here.'
+sending_email_address = 'Your email address goes here.'
+sending_email_address_password = 'Your email password goes here.'
+receiving_email_address = 'A recipient email address goes here. This can be the same as sending_email_address to send emails to yourself.'
