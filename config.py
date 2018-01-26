8fbf648eb60ef589101f3b66644e83d8e973683f
brettvanderwerff <34728357+brettvanderwerff@users.noreply.github.com>
2018-01-25 19:44:01 -0800

    Update config.py
    Made a Config class that imports configurations from a JSON file.

--- config.py
+++ config.py
@@ -2,8 +2,21 @@
 and the ebay_buy_it_now_search_tool.
 '''
 
-api_key = 'Your ebay API key goes here.'
-sending_email_address = 'Your email address goes here.'
-sending_email_address_password = 'Your email password goes here.'
-receiving_email_address = 'A recipient email address goes here. This can be the same as sending_email_address to send emails to yourself.'
-seconds_in_a_day = 86400
+import json
+
+class Config():
+    '''The Config class reads a JSON file that contains the configuration values for the 'send_email' module and
+    the 'ebay_buy_it_now_search_tool.py' module.'''
+    def __init__(self):
+        with open('config.json', 'r') as json_object:
+            config = json.load(json_object)
+            self.api_key = config['api_key']
+            self.sending_email_address = config['sending_email_address']
+            self.sending_email_address_password = config['sending_email_address_password']
+            self.receiving_email_address = config['receiving_email_address']
+            self.wait_time = int(config['wait_time'])
+
+config = Config()
+
+
+
